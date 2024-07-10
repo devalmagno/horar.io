9e76c75240a8eb5b42df04fb2f061a8ed27151f1
devalmagno <devalmagno@gmail.com>
2024-07-10 10:54:31 -0300

    Update Sidebar file

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { FiDownload } from "react-icons/fi";
+import AddDiscipline from "./AddDiscipline";
 
 export default function Sidebar() {
+  const [isAddingDiscipline, setIsAddingDiscipline] = useState(false);
+
   return (
-    <div className="py-5 sm:w-[400px]">
-      <div className="w-full flex px-5 gap-5 text-[#fff]">
-        <button className="bg-default h-10 w-full rounded flex items-center justify-center text-sm shadow-md">Adicionar Disciplina</button>
-        <button className="bg-default h-10 w-12 rounded flex items-center justify-center shadow-md">
-          <FiDownload />
-        </button>
-      </div>
+    <div className="p-5 sm:w-[400px]">
+      {isAddingDiscipline ? (
+        <AddDiscipline setIsAddingDiscipline={setIsAddingDiscipline} />
+      ) : (
+        <div className="w-full flex gap-5 text-[#fff]">
+          <button
+            className="bg-default h-10 w-full rounded flex items-center justify-center text-sm shadow-md"
+            onClick={() => setIsAddingDiscipline(true)}
+          >
+            Adicionar Disciplina
+          </button>
+          <button className="bg-default h-10 w-12 rounded flex items-center justify-center shadow-md">
+            <FiDownload />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
